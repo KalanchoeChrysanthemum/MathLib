957e31cc2563bb9e597b8a98a794ed2719719e51
KalanchoeChrysanthemum <157724777+KalanchoeChrysanthemum@users.noreply.github.com>
2025-02-08 00:38:54 -0500

    Adding vectors and basic vector functionality

--- lib/linalg/linalg.go
+++ lib/linalg/linalg.go
@@ -1,6 +1,86 @@
 package linalg
 
-// Placeholder
-func Test() string {
-	return "In linalg"
+import (
+	"math"
+	"fmt"
+)
+
+type Vector struct {
+	Components []float64
 }
+
+func VecFrom(components ...float64) Vector {
+	return Vector{Components: components}
+}
+
+func (v Vector) Dim() int {
+	return len(v.Components)
+}
+
+func (v Vector) Mag() float64 {
+	sum := 0.0
+	for _, c := range v.Components {
+		sum += c * c
+	}
+	return math.Sqrt(sum)
+}
+
+func (v Vector) Norm() Vector {
+	mag := v.Mag()
+	if mag == 0 {
+		return VecFrom(make([]float64, v.Dim())...)
+	}
+
+	norm := make([]float64, v.Dim())
+	for i, c := range v.Components {
+		norm[i] = c / mag
+	}
+	return VecFrom(norm...)
+}
+
+func (v *Vector) NormInPlace() {
+	mag := v.Mag()
+	if mag == 0 {
+		return
+	}
+	for i := range v.Components {
+		v.Components[i] /= mag
+	}
+}
+
+func (v Vector) Dot(o Vector) (float64, error) {
+	if v.Dim() != o.Dim() {
+		return 0, fmt.Errorf("Supplied vector dimension does not match expected. Supplied: %d -- Expected: %d", v.Dim(), o.Dim())
+	}
+	
+	res := 0.0
+	for i, c := range v.Components {
+		res += c * o.Components[i]
+	}
+	return res, nil
+}
+
+func (v Vector) Add(o Vector) (Vector, error) {
+	if v.Dim() != o.Dim() {
+		return Vector{}, fmt.Errorf("Supplied vector dimension does not match expected. Supplied: %d -- Expected: %d", v.Dim(), o.Dim())
+	}
+
+	sum := make([]float64, v.Dim())
+	for i := range v.Components {
+		sum[i] = v.Components[i] + o.Components[i]
+	}
+	return VecFrom(sum...), nil
+}
+
+func (v Vector) Subtract(o Vector) (Vector, error) {
+	if v.Dim() != o.Dim() {
+		return Vector{}, fmt.Errorf("Supplied vector dimension does not match expected. Supplied: %d -- Expected: %d", v.Dim(), o.Dim())
+	}
+
+	sub := make([]float64, v.Dim())
+	for i := range v.Components {
+		sub[i] = v.Components[i] - o.Components[i]
+	}
+	return VecFrom(sub...), nil
+}
+
--- src/main.go
+++ src/main.go
@@ -8,7 +8,33 @@ import (
 
 func main() {
 	fmt.Println("In main")
-	fmt.Println(linalg.Test())
 	const x = consts.T
-	fmt.Printf("Const T: %d", x)
+	fmt.Printf("Const T: %d\n", x)
+
+	// Test linear algebra package
+	v1 := linalg.VecFrom(2, 4, 6)
+	fmt.Println("v1: ", v1)
+	v2 := linalg.VecFrom(1, 3, 5)
+	fmt.Println("v2: ", v2)
+
+	fmt.Println("v1 norm: ", v1.Norm())
+
+	v3, err := v1.Add(v2)
+
+	if err != nil {
+		fmt.Println("Shouldn't be possible")
+	}
+	
+	fmt.Println("v1 + v2: ", v3)
+	v3.NormInPlace()
+	fmt.Println("v3 norm in place: ", v3)
+
+	v4, err := v1.Dot(v2)
+
+	if err != nil {
+		fmt.Println("Shouldn't be possible")
+	}
+
+	fmt.Println("v2 dot v2: ", v4)
+	
 }
